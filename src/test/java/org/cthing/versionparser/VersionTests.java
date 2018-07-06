a7fff0c064841f29a4d87362393c0b7db7f1b154
Baron Roberts <baron@cthing.com>
2018-07-05 23:52:03 -0700

    Expand static star imports

--- src/test/java/org/cthing/versionparser/VersionTests.java
+++ src/test/java/org/cthing/versionparser/VersionTests.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class VersionTests {
