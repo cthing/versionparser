6345edafc65f28582910edb782ad8804cce51435
Baron Roberts <baron@cthing.com>
2024-05-25 13:42:06 -0700

    Use ProjectVersion for versioning.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
  
 - Changed JSR-305 dependency from `implementation` to `api`
+- Use cthing-projectversion library `ProjectVersion` object for project version
 
 ## [4.2.1]
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.cthing.projectversion.BuildType
+import org.cthing.projectversion.ProjectVersion
 import com.github.spotbugs.snom.Effort
 import com.github.spotbugs.snom.Confidence
 import java.text.SimpleDateFormat
@@ -21,12 +23,16 @@ plugins {
     alias(libs.plugins.versions)
 }
 
-val baseVersion = "4.3.0"
-val isSnapshot = true
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.cthingProjectVersion)
+    }
+}
 
-val isCIServer = System.getenv("CTHING_CI") != null
-val buildNumber = if (isCIServer) System.currentTimeMillis().toString() else "0"
-version = if (isSnapshot) "$baseVersion-$buildNumber" else baseVersion
+version = ProjectVersion("4.3.0", BuildType.snapshot)
 group = "org.cthing"
 description = "Parses version numbers, ranges and constraints in a variety of formats."
 
@@ -205,7 +211,8 @@ publishing {
         }
     }
 
-    val repoUrl = if (isSnapshot) findProperty("cthing.nexus.snapshotsUrl") else findProperty("cthing.nexus.candidatesUrl")
+    val repoUrl = if ((version as ProjectVersion).isSnapshotBuild)
+        findProperty("cthing.nexus.snapshotsUrl") else findProperty("cthing.nexus.candidatesUrl")
     if (repoUrl != null) {
         repositories {
             maven {
